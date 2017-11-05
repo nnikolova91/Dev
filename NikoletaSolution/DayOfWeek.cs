b88d2d2a071b421a69ca4afdf6e9e617bf8c98b9
Sarkis Haralampiev <haralampiev9210@gmail.com>
2017-11-05 09:52:51 +0200

    commit

---
+++ NikoletaSolution/DayOfWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NikoletaSolution
+{
+    public class DayOfWeek
+    {
+        private int age;
+
+        public int Age { get; set; }
+    
+        public void PrintDayOfWeek(string date)
+        {
+            Console.WriteLine((Convert.ToDateTime(date)).DayOfWeek);
+        }
+    }
+}
