27bfd6c907ade760c00eb5696017dde57778e654
nnikolova91 <n.nikolova.it@gmail.com>
2018-02-10 20:03:04 +0200

    commit

---
+++ Regex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "([a-z]+)([0-9]+)";
+            string input = "textabv1 asd TT TTT T wew4 444sxds a4 aaa";
+
+            Regex r = new Regex(pattern);
+            //Match match = r.Match(input);
+
+            MatchCollection matches = r.Matches(input);
+
+            int sum = 0;
+            foreach (Match m in matches)
+            {
+                Console.WriteLine(m);
+                sum += int.Parse(m.Groups[2].Value);
+            }
+
+            //Console.WriteLine(match.Value);
+        }
+    }
+}
